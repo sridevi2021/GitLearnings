3b886620abcc6c1f475bda34c0cb68f5e7fbf639
sridevi2021 <sangadis121@gmail.com>
2023-09-07 22:36:02 +0100

    This is my first baseline code with Git Learning

---
+++ Gitworkflow/src/main/java/learning/GitLearning1.java
@@ -0,0 +1,10 @@
+package learning;
+
+public class GitLearning1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I'm the first code in Git learnings repository");
+	}
+
+}
